5181e5872e60f428c5bd376cb18a5e0e19720465
Alexey Shishkin <Lech.x32@gmail.com>
2020-10-18 15:55:55 +0400

    Simple project created

---
+++ app/src/main/java/com/example/studentass/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.studentass
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Debug
+import android.os.Message
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private var editTextName : EditText? = null
+    private var editTextPincode : EditText? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editTextName = findViewById(R.id.editTextName)
+        editTextPincode = findViewById(R.id.editTextPincode)
+    }
+
+    fun onButtonLoginClick(view: View) {
+        var name : String = editTextName?.text.toString()
+        var pincode : String = editTextPincode?.text.toString()
+
+        Toast.makeText(this, "Name: " + name + ";   Pincode: " + pincode, Toast.LENGTH_SHORT).show()
+    }
+
+
+}
