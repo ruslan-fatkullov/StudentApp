295420714400299f33eb5814aa4ae59ec3e973e5
Alexey Shishkin <Lech.x32@gmail.com>
2020-10-18 20:02:33 +0400

    Login quick fix

--- app/src/main/java/com/example/studentass/MainActivity.kt
+++ app/src/main/java/com/example/studentass/MainActivity.kt
@@ -9,30 +9,30 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private var editTextName : EditText? = null
+    private var editTextEmail : EditText? = null
     private var editTextPassword : EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editTextName = findViewById(R.id.editTextName)
+        editTextEmail = findViewById(R.id.editTextEmail)
         editTextPassword = findViewById(R.id.editTextPassword)
     }
 
     fun onButtonLoginClick(view: View) {
         try {
-            var name : String = editTextName?.text.toString()
+            var email : String = editTextEmail?.text.toString()
             var password : String = editTextPassword?.text.toString()
 
-            if (name.length < 1)
-                throw Exception("Не указано имя пользователя")
-            if (name.indexOf(' ', 0, true) != -1)
-                throw Exception("Имя пользователя содержит недопустимые символы")
+            if (email.length < 1)
+                throw Exception("Не указан адрес эл. почты")
+            if (email.indexOf(' ', 0, true) != -1)
+                throw Exception("Адрес эл. почты содержит недопустимые символы")
             if (password.length < 1)
                 throw Exception("Не указан пароль")
 
-            var message : String = "Name: " + name + "    Pincode: " + password
+            var message : String = "Почта: " + email + "    Пароль: " + password
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
         catch (e: Exception) {
