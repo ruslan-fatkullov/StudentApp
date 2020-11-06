93bdf91bc2a4ff5a96e68c4d5956fa36786ef527
Alexey Shishkin <Lech.x32@gmail.com>
2020-11-06 18:30:05 +0400

    Bottom view panel and first handlers

--- app/src/main/java/com/example/studentass/MainActivity2.kt
+++ app/src/main/java/com/example/studentass/MainActivity2.kt
@@ -7,15 +7,40 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import com.example.studentass.fragments.NotificationsFragment
+import com.example.studentass.fragments.ScheduleFragment
+import com.example.studentass.fragments.SubjectsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main2.*
 import java.net.HttpURLConnection
 import java.net.URL
 import kotlin.concurrent.thread
 
 class MainActivity2 : AppCompatActivity() {
     var textViewMessage : TextView? = null
+    var bnv : BottomNavigationView? = null
 
     companion object {
         const val MESSAGE = "message"
+
+        fun sendGet(url : String) : String {
+            val url = URL(url)
+            var responceStr : String;
+            with(url.openConnection() as HttpURLConnection) {
+                requestMethod = "GET"  // optional default is GET
+
+                responceStr = inputStream.use { it.reader().use { reader -> reader.readText() } }
+                //println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
+
+                /*inputStream.bufferedReader().use {
+                    it.lines().forEach { line ->
+                        println(line)
+                    }
+                }*/
+            }
+            return responceStr;
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +48,22 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         //textViewMessage = findViewById(R.id.textViewMessage)
+        bnv = findViewById(R.id.bottomNavigationView)
+
+        val scheduleFragment = ScheduleFragment()
+        val subjectsFragment = SubjectsFragment()
+        val notificationsFragment = NotificationsFragment()
+
+        makeCurrentFragment(scheduleFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.bnv_schedule -> makeCurrentFragment(scheduleFragment)
+                R.id.bnv_subjects -> makeCurrentFragment(subjectsFragment)
+                R.id.bnv_notifications -> makeCurrentFragment(notificationsFragment)
+            }
+            true
+        }
 
 
         val message = intent.getStringExtra(MESSAGE)
@@ -38,21 +79,9 @@ class MainActivity2 : AppCompatActivity() {
         startActivity(intentActivity)
     }
 
-    fun sendGet(url : String) : String {
-        val url = URL(url)
-        var responceStr : String;
-        with(url.openConnection() as HttpURLConnection) {
-            requestMethod = "GET"  // optional default is GET
-
-            responceStr = inputStream.use { it.reader().use { reader -> reader.readText() } }
-            //println("\nSent 'GET' request to URL : $url; Response Code : $responseCode")
-
-            /*inputStream.bufferedReader().use {
-                it.lines().forEach { line ->
-                    println(line)
-                }
-            }*/
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            commit()
         }
-        return responceStr;
-    }
 }
---
+++ app/src/main/java/com/example/studentass/fragments/SubjectsFragment.kt
@@ -0,0 +1,82 @@
+package com.example.studentass.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.studentass.MainActivity2
+import com.example.studentass.R
+import kotlinx.android.synthetic.main.fragment_subjects.*
+import kotlin.concurrent.thread
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SubjectsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SubjectsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_subjects, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mainHandler : Handler = Handler(Looper.getMainLooper())
+        thread {
+            val text = MainActivity2.sendGet("https://4b7af1df-c62e-49e5-b0a5-929837fb7e36.mock.pstmn.io/group/?name_group=testgrp")
+            val gsRunnable = Runnable {
+                subjectsTestTV?.text = text
+            }
+            mainHandler.post(gsRunnable)
+        }
+
+
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SubjectsFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SubjectsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
